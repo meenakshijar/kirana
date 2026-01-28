70a06e28b1342338c622d66db2b27707c091d78e
Meenakshi Rajesh <meenakshirajesh@JAR-LT-242.local>
2026-01-28 14:21:24 +0530

    added unit test cases

--- src/main/java/com/example/kirana/service/ReportService.java
+++
@@ -1,8 +0,0 @@
-package com.example.kirana.service;
-
-import com.example.kirana.dto.ReportRequest;
-import com.example.kirana.dto.ReportResponse;
-
-public interface ReportService {
-    ReportResponse getSummary(ReportRequest request);
-}
